324eaf9812e8da82f502a5f31de8fe53660f30d0
bezumnui <bezumnui.mistikgt@gmail.com>
2024-06-15 22:05:40 +0400

    first commit

---
+++ game_macros.c
@@ -0,0 +1,7 @@
+#include "game_macros.h"
+
+
+int32_t game_macros_main(void *p) {
+    UNUSED(p);
+    return 0;
+}
---
+++ game_macros.h
@@ -0,0 +1,12 @@
+#include <furi.h>
+#include <gui/gui.h>
+#include <gui/scene_manager.h>
+#include <gui/view_dispatcher.h>
+
+typedef struct {
+} GameMacrosContext;
+
+// entry point
+int32_t game_macros_main(void* p);
+
+void game_macros_free();
